2f0e42c67b1ac4c994b6529ad0a5d288c7c39131
d-angelotorres <dangelotorres2@gmail.com>
2025-01-11 22:07:23 -0500

    Initial commit with Fibonacci code

---
+++ src/Fibonacci.java
@@ -0,0 +1,19 @@
+public class Fibonacci {
+
+    /**
+     * Returns the nth term in the Fibonacci sequence.
+     * @param n The position of the term in the Fibonacci sequence.
+     * @return The nth term in the Fibonacci sequence.
+     */
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println("The " + n + "th term of the Fibonacci sequence is " + fibonacci(n));
+    }
+}
